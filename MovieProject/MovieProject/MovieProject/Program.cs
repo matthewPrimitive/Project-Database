d2bc9d2433018ef5583c529cd314af2c84e3766b
jamesmmatt <jamesmmatt@ksu.edu>
2019-12-01 20:16:33 -0600

    Fixing trevors mistakes out here

--- MovieProject/MovieProject/MovieProject/Program.cs
+++ MovieProject/MovieProject/MovieProject/Program.cs
@@ -16,7 +16,7 @@ namespace MovieProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
