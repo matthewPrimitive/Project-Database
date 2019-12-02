a3e9349cd2b64b0893bba48936e1dbe3be7a2ed6
jamesmmatt <jamesmmatt@E1118W08>
2019-12-02 17:21:13 -0600

    Login and Create User is a go red Fawx

--- 560GUI/560GUI/loginForm.cs
+++ 560GUI/560GUI/loginForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using MovieProject;
 using DataAccess;
 using MovieProject.Models;
-
+using System.Text;
 
 namespace _560GUI
 {
@@ -43,12 +43,13 @@ namespace _560GUI
         {
             string user = "";
 
-            user = userTextBox.Text;
+            user = userTextBox.Text.ToLower();
                 
             try
             {
                 Viewer v = viewerRepo.RetrieveViewerOnEmail(user);
-                mainForm.theUser = new currentUser(user);
+                string[] username = user.Split('@');
+                mainForm.theUser = new currentUser(username[0]);
                 mainForm.changeLabel(mainForm.theUser.userName);
             }
             catch
